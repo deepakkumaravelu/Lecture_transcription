0da6c8f36317791aece51dd12714db12c4838fa2
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-11-16 22:28:07 +0530

    Revert "env file updated"
    This reverts commit 5d583fed3efa8e948d1e1acf1b692b259f11f720.

--- aws-transcribe/transcribe-list-jobs.js
+++ aws-transcribe/transcribe-list-jobs.js
@@ -10,8 +10,6 @@ const path = require('path');
 // Configure AWS SDK
 AWS.config.update({
   region: 'ap-southeast-2',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const s3 = new AWS.S3();
