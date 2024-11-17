45fa65efc3be4977297c249f1d0fb76b63f783c1
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-11-17 08:20:53 +0530

    link updated

--- aws-transcribe-client/src/App.jsx
+++ aws-transcribe-client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     setLoading(true);
     setError('');
     try {
-      const response = await axios.get('http://localhost:3000/get-pdfs');
+      const response = await axios.get('https://lecture-transcription.onrender.com/get-pdfs');
       setPdfUrls(response.data.pdfUrls);
     } catch (err) {
       setError('Failed to fetch PDF files. Please try again.');
